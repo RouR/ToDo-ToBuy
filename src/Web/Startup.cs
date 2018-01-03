5e5778a415a6cfd7ded1a8f0ccdb802ea2828d07
RouR <RouR@users.noreply.github.com>
2018-01-04 02:45:19 +0300

    CI

---
+++ src/Web/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Web.Utils;
+
+namespace Web
+{
+    /*
+     * Install-Package Microsoft.AspNetCore.All     
+     */
+    public class Startup
+    {
+        //public Startup(IConfiguration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
+
+        //public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddSingleton<InstanceInfo>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //var configuration = app.ApplicationServices.GetService<TelemetryConfiguration>();
+            //configuration.DisableTelemetry = true;
+
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new {area="home", controller = "Hello", action = "Index" });
+            });
+        }
+    }
+}
---
+++ src/Web/Utils/InstanceInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Web.Utils
+{
+    public class InstanceInfo
+    {
+        public InstanceInfo()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; }
+        public string CodeVer = "v1";
+       
+    }
+}
