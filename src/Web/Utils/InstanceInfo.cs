ccf71b37e94ee8445393f44509365855d2e36899
RouR <rour@yandex.ru>
2018-01-03 23:30:07 +0300

    init

---
+++ src/Web/Utils/InstanceInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Web.Utils
+{
+    public class InstanceInfo
+    {
+        public InstanceInfo()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; }
+    }
+}
