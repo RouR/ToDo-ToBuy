a894f512e474a3ba1315c22e3f04896bd66d7dec
alex.d <rour@yandex.ru>
2018-10-29 23:56:35 +0300

    retries

--- src/Shared/BaseClient.cs
+++ src/Shared/BaseClient.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using DTO;
@@ -18,15 +19,60 @@ namespace Shared
             var uri = new Uri(url);
             Console.WriteLine($"{clientId.ToString()}Client Uri {uri}");
 
+            if(!int.TryParse(Environment.GetEnvironmentVariable($"apiTimeout"), out var timeout))
+                timeout = 1000;
+            
             _client = factory.CreateClient(clientId.ToString());
             _client.BaseAddress = uri;
+            _client.Timeout = TimeSpan.FromMilliseconds(timeout);
             _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
         public static void DefaultStrategy(IHttpClientBuilder builder)
         {
-            builder.AddTransientHttpErrorPolicy(p => 
-                p.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(100)));
+            if(!int.TryParse(Environment.GetEnvironmentVariable($"apiRetriesCount"), out var retries))
+                retries = 3;
+            
+            if(!int.TryParse(Environment.GetEnvironmentVariable($"apiRetriesDelayMin"), out var delayMin))
+                delayMin = 100;
+            
+            if(!int.TryParse(Environment.GetEnvironmentVariable($"apiRetriesDelayStep"), out var delayStep))
+                delayStep = 50;
+            
+            if(!int.TryParse(Environment.GetEnvironmentVariable($"apiRetriesJitterMin"), out var jitterMin))
+                jitterMin = 0;
+            
+            if(!int.TryParse(Environment.GetEnvironmentVariable($"apiRetriesJitterMax"), out var jitterMax))
+                jitterMax = 100;
+            
+            if(!int.TryParse(Environment.GetEnvironmentVariable($"apiCircuitBreakerCount"), out var circuitCount))
+                circuitCount = 5;
+            
+            if(!int.TryParse(Environment.GetEnvironmentVariable($"apiCircuitBreakerDelaySeconds"), out var circuitDelay))
+                circuitDelay = 30;
+            
+            if(!int.TryParse(Environment.GetEnvironmentVariable($"apiHandlerLifetimeSeconds"), out var lifetime))
+                lifetime = 300;
+            
+            //https://docs.microsoft.com/en-us/aspnet/core/fundamentals/http-requests?view=aspnetcore-2.1
+            //https://github.com/App-vNext/Polly
+            //https://github.com/App-vNext/Polly/wiki/Polly-and-HttpClientFactory
+            //HttpRequestException, 5XX and 408
+            var jitter = new Random(); 
+            builder
+                .AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(retries, retryAttempt  => 
+                        TimeSpan.FromMilliseconds(delayMin + retryAttempt * delayStep) 
+                        + TimeSpan.FromMilliseconds(jitter.Next(jitterMin, jitterMax)))
+                )
+                .AddTransientHttpErrorPolicy(p => p.CircuitBreakerAsync(circuitCount, TimeSpan.FromSeconds(circuitDelay)))
+                .SetHandlerLifetime(TimeSpan.FromSeconds(lifetime)) // can prevent the handler from reacting to DNS changes
+                .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler()
+                {
+                    AllowAutoRedirect = true,
+                    MaxAutomaticRedirections = 3,
+                    UseDefaultCredentials = false,
+                    AutomaticDecompression = DecompressionMethods.GZip,
+                });
         }
     }
 }
