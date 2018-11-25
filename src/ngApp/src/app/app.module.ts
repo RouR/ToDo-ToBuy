66ec70ede61f052da7a72bab3cecb444c882d6ca
alex.d <rour@yandex.ru>
2018-11-25 22:17:25 +0300

    ngApp - add material theme

--- src/ngApp/src/app/app.module.ts
+++ src/ngApp/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/ngApp/src/main.ts
+++ src/ngApp/src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import 'hammerjs';  //Some components like Slide Toggle, Slider, and Tooltip rely on a library called HammerJS to capture touch gestures.
+
 if (environment.production) {
   enableProdMode();
 }
